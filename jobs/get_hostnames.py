c3cec2eec944dd906ed9e4d76bb8795d9b73a36c
Julian N <56843080+jnaranjom@users.noreply.github.com>
2024-02-11 13:36:07 -0600

    Update get_hostnames.py

--- jobs/get_hostnames.py
+++ jobs/get_hostnames.py
@@ -25,7 +25,7 @@ class GetHostnames(Job):
         for device in devices:
             self.logger.info(f"{device.name}: {device.role}")
             self.logger.info(f"{device.name}: {device.platform}")
-            self.logger.info(f"{device.name}: {device.interfaces}")
+            self.logger.info(f"{device.name}: {device.status}")
             if device.primary_ip4:
                 self.logger.info(f"{device.name}: {device.primary_ip4}")
             else:
