49b49278f4161f33de0fd99943ec99dfb707735a
Julian N <56843080+jnaranjom@users.noreply.github.com>
2024-02-11 13:23:20 -0600

    Update get_hostnames.py

--- jobs/get_hostnames.py
+++ jobs/get_hostnames.py
@@ -1,7 +1,7 @@
 """ GET HOSTNAME OF ALL DEVICES """
 
 import os
-from nautobot.apps.jobs import Job, MultiObjectVar, register_jobs
+from nautobot.apps.jobs import Job, MultiObjectVar
 from nautobot.dcim.models.devices import Device, DeviceRedundancyGroup, DeviceType, Manufacturer, Platform, VirtualChassis
 
 class GetHostnames(Job):
@@ -24,5 +24,3 @@ class GetHostnames(Job):
         """ Main function """
         for device in devices:
             self.logger.info(f"{device.role}")
-
-register_jobs(GetHostnames)
