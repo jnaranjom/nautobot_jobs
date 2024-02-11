f02b840ed2061f6fa301c61317b847b1abaf143d
Julian N <56843080+jnaranjom@users.noreply.github.com>
2024-02-11 13:01:00 -0600

    Create get_hostnames.py

---
+++ jobs/get_hostnames.py
@@ -0,0 +1,26 @@
+""" GET HOSTNAME OF ALL DEVICES """
+
+import os
+from nautobot.apps.jobs import Job, MultiObjectVar, register_jobs
+from nautobot.dcim.models.devices import Device, DeviceRedundancyGroup, DeviceType, Manufacturer, Platform, VirtualChassis
+
+class GetHostnames(Job):
+    """ Example job definition """
+
+    devices = MultiObjectVar (
+        model=Device,
+        query_params={
+            'status': 'Active'
+        }
+    )
+
+    class Meta:
+        """ Jobs Metadata """
+        name = "Get Hostnames"
+        description = "Description of Get Hostnames job"
+        dryrun_default = True
+
+    def run(self, devices):
+        """ Main function """
+        for device in devices:
+            self.logger.info(f"{device.role}")
