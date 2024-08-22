ba61cba3e41a2f36597b80ad60f73e57991e82d7
Julian N. <56843080+jnaranjom@users.noreply.github.com>
2024-08-22 13:34:49 -0500

    Update __init__.py

--- jobs/__init__.py
+++ jobs/__init__.py
@@ -1,6 +1,6 @@
 """ REGISTER JOBS HERE """
 
 from nautobot.apps.jobs import Job, register_jobs
-from .get_hostnames import GetHostnames
+from get_hostnames import GetHostnames
 
 register_jobs(GetHostnames)
