ba1536dd705f5461510342c4bd0cb41602406e77
Julian N. <56843080+jnaranjom@users.noreply.github.com>
2024-08-23 16:09:36 -0500

    Update get_device_details.py

--- jobs/get_device_details.py
+++ jobs/get_device_details.py
@@ -9,6 +9,10 @@ class GetHostnames(Job):
 
     tenant = ObjectVar(model=Tenant)
 
+    devices = MultiObjectVar(
+        model=Device, query_params={"status": "Active", "tenant": "$tenant"}
+    )
+
     class Meta:
         """Jobs Metadata"""
 
@@ -16,13 +20,21 @@ class GetHostnames(Job):
         description = "Job to retrieve device info"
         dryrun_default = True
 
-    def run(self, tenant):
+    def run(self, devices):
         """_summary_
 
         Args:
             devices (_type_): _description_
         """
-        self.logger.info("%s", tenant)
+
+        for device in devices:
+            self.logger.info("%s", device.name)
+            self.logger.info(f'{device.name}: {device.platform}')
+            self.logger.info(f'{device.name}: {device.status}')
+            if device.primary_ip4:
+                self.logger.info(f'{device.name}: {device.primary_ip4}')
+            else:
+                self.logger.info(f'Unable to find Primary IPv4 for {device.name}')
 
 
 register_jobs(GetHostnames)
