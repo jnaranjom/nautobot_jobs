481914284f2636c74c5dc48bd59c3cf5d3f5b286
Net Admin <netadmin@devserver.localdomain>
2024-08-29 17:23:39 +0000

    Add select MGMT switch

--- jobs/set_mgmt_ip.py
+++ jobs/set_mgmt_ip.py
@@ -3,7 +3,8 @@
 from nautobot.apps.jobs import Job, ObjectVar, MultiObjectVar, register_jobs
 from nautobot.dcim.models.locations import Location, LocationType
 from nautobot.ipam.models import IPAddress, Prefix, Namespace
-from nautobot.dcim.models import Device, Interface
+from nautobot.dcim.models import Device, Interface, Cable
+from django.contrib.contenttypes.models import ContentType
 from nautobot.extras.models import Status, Role
 
 
@@ -42,40 +43,42 @@ class SetManagementIP(Job):
 
         mgmt_ip_status = Status.objects.get(name="Reserved")
 
+        mgmt_cable_status = Status.objects.get(name="Connected")
 
-        #TODO: FAIL IN NUM OF DEVICES IS GREATER THAN AVAILABLE MGMT INT
+        termination_type = ContentType.objects.get(app_label="dcim", model="interface")
+
+        # TODO: FAIL IN NUM OF DEVICES IS GREATER THAN AVAILABLE MGMT INT
         for idx, device in enumerate(devices):
             device_mgmt_int = device.interfaces.get(mgmt_only=True)
 
-            print(device.name, device_mgmt_int.name, mgmt_switch.name, mgmt_interfaces[idx].name)
+            mgmt_cable = Cable.objects.get_or_create(
+                termination_a_type=termination_type,
+                termination_a_id=device_mgmt_int.id,
+                termination_b_type=termination_type,
+                termination_b_id=mgmt_interfaces[idx].id,
+                status=mgmt_cable_status,
+            )
+
+            mgmt_cable.validated_save()
 
             ipaddress = mgmt_prefix.get_first_available_ip()
 
-            mgmt_ip = IPAddress(address=ipaddress,namespace=mgmt_prefix.namespace,type="host",status=mgmt_ip_status)
+            mgmt_ip = IPAddress(
+                address=ipaddress,
+                namespace=mgmt_prefix.namespace,
+                type="host",
+                status=mgmt_ip_status,
+            )
 
             mgmt_ip.validated_save()
 
             device_mgmt_int.ip_addresses.add(mgmt_ip)
 
-            device_mgmt_int.description = mgmt_switch.name + mgmt_interfaces[idx].name
+            device_mgmt_int.description = (
+                mgmt_switch.name + " " + mgmt_interfaces[idx].name
+            )
 
             device_mgmt_int.validated_save()
-
-
-        # for myinterface in myinterfaces:
-        #     if myinterface.device in devices:
-
-        #         myipaddresses = IPAddress.objects.filter(
-        #             role__name="network:management", status__name="Reserved"
-        #         )
-
-        #         myipaddress = myipaddresses[0]
-        #         myipaddress.status = Status.objects.get(name="Active")
-        #         myipaddress.validated_save()
-
-        #         myinterface.ip_addresses.add(myipaddress)
-        #         myinterface.description = "MGMT"
-        #         myinterface.validated_save()
 
 
 register_jobs(SetManagementIP)
