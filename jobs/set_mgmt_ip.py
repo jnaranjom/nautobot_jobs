0cec35f83df8c4b6b21d161083ca551ccf0b391c
Net Admin <netadmin@devserver.localdomain>
2024-08-29 23:01:22 +0000

    Clean job

--- jobs/set_mgmt_ip.py
+++ jobs/set_mgmt_ip.py
@@ -1,11 +1,11 @@
 """ SET MANAGEMENT ON DEVICES """
 
 from nautobot.apps.jobs import Job, ObjectVar, MultiObjectVar, register_jobs
-from nautobot.dcim.models.locations import Location, LocationType
-from nautobot.ipam.models import IPAddress, Prefix, Namespace
-from nautobot.dcim.models import Device, Interface, Cable
+from nautobot.dcim.models.locations import Location
+from nautobot.dcim.models import Device, Cable
+from nautobot.ipam.models import IPAddress, Prefix
 from django.contrib.contenttypes.models import ContentType
-from nautobot.extras.models import Status, Role
+from nautobot.extras.models import Status
 
 
 class SetManagementIP(Job):
@@ -24,26 +24,25 @@ class SetManagementIP(Job):
     description = "Job to set the Management on the devices"
     dryrun_default = True
 
-    def run(self, location, mgmt_switch, devices):
+    def run(self, mgmt_switch, devices):
         """Main function"""
 
         planned_status = Status.objects.get(name="Planned")
         active_status = Status.objects.get(name="Active")
         mgmt_ip_status = Status.objects.get(name="Reserved")
+        mgmt_cable_status = Status.objects.get(name="Connected")
 
         mgmt_interfaces = mgmt_switch.interfaces.filter(status=planned_status)
         mgmt_prefix = Prefix.objects.get(role__name="network:management")
-        mgmt_cable_status = Status.objects.get(name="Connected")
         termination_type = ContentType.objects.get(app_label="dcim", model="interface")
 
-        # TODO: FAIL IN NUM OF DEVICES IS GREATER THAN AVAILABLE MGMT INT
         for idx, device in enumerate(devices):
 
             device_mgmt_int = device.interfaces.get(mgmt_only=True)
 
             if device_mgmt_int.ip_addresses.first():
                 self.logger.info(
-                    f"Device: {device.name}, Interface {device_mgmt_int.name} has an IP assigned already"
+                    f"Device: {device.name}, Interface: {device_mgmt_int.name} has an IP assigned already"
                 )
 
             else:
@@ -70,7 +69,7 @@ class SetManagementIP(Job):
 
             if device_mgmt_int.connected_endpoint:
                 self.logger.info(
-                    f"Device: {device.name}, Interface {device_mgmt_int.name} has an active connection"
+                    f"Device: {device.name}, Interface: {device_mgmt_int.name} has an active connection"
                 )
             else:
                 mgmt_cable, _ = Cable.objects.get_or_create(
