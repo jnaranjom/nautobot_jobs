b61f0c4ac9feb9166b70ccbc5b67a709cc96cf86
Net Admin <netadmin@devserver.localdomain>
2024-08-30 03:09:14 +0000

    Clean job

--- jobs/set_mgmt_ip.py
+++ jobs/set_mgmt_ip.py
@@ -36,55 +36,60 @@ class SetManagementIP(Job):
         mgmt_prefix = Prefix.objects.get(role__name="network:management")
         termination_type = ContentType.objects.get(app_label="dcim", model="interface")
 
-        for idx, device in enumerate(devices):
+        if len(mgmt_interfaces) >= len(devices):
+            for idx, device in enumerate(devices):
 
-            device_mgmt_int = device.interfaces.get(mgmt_only=True)
+                device_mgmt_int = device.interfaces.get(mgmt_only=True)
 
-            if device_mgmt_int.ip_addresses.first():
-                self.logger.info(
-                    f"Device: {device.name}, Interface: {device_mgmt_int.name} has an IP assigned already"
-                )
+                if device_mgmt_int.ip_addresses.first():
+                    self.logger.info(
+                        f"Device: {device.name}, Interface: {device_mgmt_int.name} has an IP assigned already"
+                    )
 
-            else:
-                ipaddress = mgmt_prefix.get_first_available_ip()
+                else:
+                    ipaddress = mgmt_prefix.get_first_available_ip()
 
-                mgmt_ip = IPAddress(
-                    address=ipaddress,
-                    namespace=mgmt_prefix.namespace,
-                    type="host",
-                    status=ipaddr_status,
-                )
+                    mgmt_ip = IPAddress(
+                        address=ipaddress,
+                        namespace=mgmt_prefix.namespace,
+                        type="host",
+                        status=ipaddr_status,
+                    )
 
-                mgmt_ip.validated_save()
+                    mgmt_ip.validated_save()
 
-                device_mgmt_int.ip_addresses.add(mgmt_ip)
-                device_mgmt_int.description = (
-                    f"{mgmt_switch.name}--{mgmt_interfaces[idx].name}"
-                )
+                    device_mgmt_int.ip_addresses.add(mgmt_ip)
+                    device_mgmt_int.description = (
+                        f"{mgmt_switch.name}--{mgmt_interfaces[idx].name}"
+                    )
 
-                device_mgmt_int.validated_save()
+                    device_mgmt_int.validated_save()
 
-                device.primary_ip4 = mgmt_ip
-                device.validated_save()
+                    device.primary_ip4 = mgmt_ip
+                    device.validated_save()
 
-            if device_mgmt_int.connected_endpoint:
-                self.logger.info(
-                    f"Device: {device.name}, Interface: {device_mgmt_int.name} has an active connection"
-                )
-            else:
-                mgmt_cable, _ = Cable.objects.get_or_create(
-                    termination_a_type=termination_type,
-                    termination_a_id=device_mgmt_int.id,
-                    termination_b_type=termination_type,
-                    termination_b_id=mgmt_interfaces[idx].id,
-                    status=cable_status,
-                )
+                if device_mgmt_int.connected_endpoint:
+                    self.logger.info(
+                        f"Device: {device.name}, Interface: {device_mgmt_int.name} has an active connection"
+                    )
+                else:
+                    mgmt_cable, _ = Cable.objects.get_or_create(
+                        termination_a_type=termination_type,
+                        termination_a_id=device_mgmt_int.id,
+                        termination_b_type=termination_type,
+                        termination_b_id=mgmt_interfaces[idx].id,
+                        status=cable_status,
+                    )
 
-                mgmt_cable.validated_save()
+                    mgmt_cable.validated_save()
 
-                mgmt_interfaces[idx].status = active_status
+                    mgmt_interfaces[idx].status = active_status
 
-                mgmt_interfaces[idx].validated_save()
+                    mgmt_interfaces[idx].validated_save()
+        else:
+            self.logger.info(
+                f"Not enough interfaces available in the management switch {mgmt_switch} selected. Only {len(mgmt_interfaces)} available. But {len(devices)} need MGMT configuration."
+            )
 
 
 register_jobs(SetManagementIP)
