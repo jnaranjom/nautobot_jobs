048fea4a08f070f816d78e91604bc3fb55e58362
Net Admin <netadmin@devserver.localdomain>
2024-09-03 15:57:08 +0000

    Create Branch deployment job

--- jobs/deploy_branch_small.py
+++ jobs/deploy_branch_small.py
@@ -74,7 +74,20 @@ class DeployBranchSmall(Job):
         )
 
         # Connect branch devices
-        connect_cable_endpoints(router_interface.id, switch_interface.id)
+        # connect_cable_endpoints(router_interface.id, switch_interface.id)
+
+        termination_type = ContentType.objects.get(app_label="dcim", model="interface")
+        cable_status = Status.objects.get(name="Connected")
+
+        connect_cable, _ = Cable.objects.get_or_create(
+            termination_a_type=termination_type,
+            termination_a_id=router_interface.id,
+            termination_b_type=termination_type,
+            termination_b_id=switch_interface.id,
+            status=cable_status,
+        )
+
+        connect_cable.validated_save()
 
         router_interface.status = active_status
         router_interface.description = (
