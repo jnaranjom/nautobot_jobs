1a22db8fb6ae48ceb375cad6c385832f33b3dfeb
Net Admin <netadmin@devserver.localdomain>
2024-09-04 21:12:44 +0000

    Add BGP routing instance

--- jobs/deploy_branch_small.py
+++ jobs/deploy_branch_small.py
@@ -11,7 +11,12 @@ from .status_helper import find_status_uuid
 from .ipaddress_helper import create_ipaddr
 from django.contrib.contenttypes.models import ContentType
 from nautobot.dcim.models.device_components import Interface
-from nautobot_bgp_models.models import AutonomousSystem, BGPRoutingInstance, PeerEndpoint, Peering
+from nautobot_bgp_models.models import (
+    AutonomousSystem,
+    BGPRoutingInstance,
+    PeerEndpoint,
+    Peering,
+)
 
 
 class DeployBranchSmall(Job):
