b497b47c375a99400fe67d73b9472632f6cf3eaf
Net Admin <netadmin@devserver.localdomain>
2024-09-13 21:16:41 +0000

    Import locations job

---
+++ jobs/ssot_import.py
@@ -0,0 +1,28 @@
+""" JOB TO IMPORT OBJECTS FROM CMDB """
+
+from nautobot.apps.jobs import Job
+import requests
+
+
+class ImportLocations(Job):
+    """Import Locations from CMDB"""
+
+
+    class Meta:
+        """Jobs Metadata"""
+
+    name = "Get List of locations from the CMDB"
+    description = "Job to read the locations from the CMDB"
+    dryrun_default = True
+
+    def run(self):
+        """Main function"""
+
+        locations = requests.get('http://127.0.0.1:8000/api/v1/locations')
+
+        self.logger.info(
+                        f"Locations on CMDB {locations}"
+                    )
+
+
+register_jobs(ImportLocations)
