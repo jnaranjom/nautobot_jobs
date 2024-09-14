65d2db4685dba44b722684642acfe340532e715f
Net Admin <netadmin@devserver.localdomain>
2024-09-14 04:14:29 +0000

    Add Check if devices exist

--- jobs/ssot_import.py
+++ jobs/ssot_import.py
@@ -37,4 +37,35 @@ class ImportLocations(Job):
                 )
 
 
-register_jobs(ImportLocations)
+class ImportDevices(Job):
+    """Import Devices from CMDB"""
+
+    class Meta:
+        """Jobs Metadata"""
+
+    name = "Get List of devices from the CMDB"
+    description = "Job to read the devices from the CMDB"
+    dryrun_default = True
+
+    def run(self):
+        """Main function"""
+
+        devices = requests.get("http://192.168.2.245:8000/api/v1/devices")
+        device_list = devices.json()
+
+        for device in device_list:
+            self.logger.info(f" Checking Device: {device['name']}")
+            try:
+                existing_devices = Device.objects.get(
+                    name=device["name"],
+                    tenant__name=device["tenant"],
+                    location__name=device["location"],
+                )
+                self.logger.info(f" Device {device['name']} found")
+            except:
+                self.logger.info(
+                    f" Device {device['name']} not found, will add new device."
+                )
+
+
+register_jobs(ImportLocations, ImportDevices)
