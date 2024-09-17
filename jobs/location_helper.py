5ddefbed751eef25415cfe395d992dc1dec810bb
Net Admin <netadmin@devserver.localdomain>
2024-09-17 22:57:24 +0000

    Add Location helper

--- jobs/location_helper.py
+++ jobs/location_helper.py
@@ -2,10 +2,11 @@
 
 from nautobot.dcim.models.locations import Location, LocationType
 from .status_helper import find_status_uuid
+from nautobot.tenancy.models import Tenant
 from django.core.exceptions import ValidationError
 
 
-def create_location(location_name, location_type, location_tenant, location_parent):
+def create_location(loc_name, loc_type, loc_tenant, loc_parent):
     """Function to create a new Location
 
     Args:
@@ -15,12 +16,13 @@ def create_location(location_name, location_type, location_tenant, location_pare
     """
     try:
         staging_status = find_status_uuid("Staging")
-        parent = Location.objects.get(name=location_parent)
-        location_type = LocationType.objects.get(name=location_type)
+        location_parent = Location.objects.get(name=loc_parent)
+        location_tenant = Tenant.objects.get(name=loc_tenant)
+        location_type = LocationType.objects.get(name=loc_type)
 
         new_location = Location(
             location_type=location_type,
-            name=location_name,
+            name=loc_name,
             status=staging_status,
             tenant=location_tenant,
             parent=location_parent,
